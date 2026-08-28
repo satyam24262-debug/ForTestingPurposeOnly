e81c383bac3ca860ca0e8adc8133d91a3bbba539
satyam24262-debug <satyam.24262@knit.ac.in>
2026-08-28 12:24:32 +0530

    second commit

--- SchPractice/index.js
+++ SchPractice/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const  connectDB  = require("./db/connection");
+const connectDB = require("./db/connection");
 const order = require("./models/order");
 const product = require("./models/product");
 const user = require("./models/user");
@@ -10,31 +10,22 @@ const app = express();
 
 app.use(cors("*"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-    connectDB();
+app.use(express.urlencoded({ extended: true }));
+connectDB();
 
+app.get("/", async (req, res) => {
+  const Order = await order
+    .findById("6a8c6029760f2eb0a3cc6397")
+    .populate("user")
+    .populate("product");
 
+  console.log(Order);
 
+  res.status(200).json({
+    message: "Lo main fir se a gya.",
+  });
+});
 
-app.get("/", async (req,res)=>{
-
-
-const Order = await order.findById("6a8c6029760f2eb0a3cc6397")
-  .populate("user")
-  .populate("product");
-
-    console.log(Order);
-
-  
-
-    res.status(200).json({
-        message:"hello frontend"
-    })
-})
-
-
-
-app.listen(4000,()=>{
-    console.log(`server is running at http://localhost:4000`);
-
-})
+app.listen(4000, () => {
+  console.log(`server is running at http://localhost:4000`);
+});
