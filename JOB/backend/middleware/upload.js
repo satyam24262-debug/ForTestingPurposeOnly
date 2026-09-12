1f2d79e0a584c5fc09d153b87a225ef05cdd38bf
satyam24262-debug <satyam.24262@knit.ac.in>
2026-09-12 09:40:18 +0530

    add cloud storage

--- JOB/backend/middleware/upload.js
+++ JOB/backend/middleware/upload.js
@@ -1,17 +1,41 @@
+// const multer = require("multer");
+// const fs = require("fs");
+// const path = require("path");
+
+// const uploadDirectory = path.join(__dirname, "uploads");
+// fs.mkdirSync(uploadDirectory, { recursive: true });
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadDirectory);
+//   },
+
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
-const uploadDirectory = path.join(__dirname, "uploads");
-fs.mkdirSync(uploadDirectory, { recursive: true });
+// Cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDirectory);
-  },
-
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+// Storage setup (no local folder needed)
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads", // Cloudinary folder name
+    allowed_formats: ["jpg", "png", "pdf"], // optional
   },
 });
 
