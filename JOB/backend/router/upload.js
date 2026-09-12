8349129062c68e85b55ed010e614c684430c0a8f
satyam24262-debug <satyam.24262@knit.ac.in>
2026-09-12 10:15:42 +0530

    test6

--- JOB/backend/router/upload.js
+++ JOB/backend/router/upload.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const upload = require("./middleware/upload");
+const upload = require("../middleware/upload");
 const cloudinary = require("cloudinary").v2;
 
 const Uploadrouter = express.Router();
